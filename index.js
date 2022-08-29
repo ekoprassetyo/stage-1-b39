25f6ce3582b2dd96658f45f5d109ad515732ffe7
ekoprassetyo <ekop2331@@gmail.com>
2022-08-29 10:33:31 +0700

    day-10

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express') // import express
+const db = require('./connection/db')
+const bcrypt = require('bcrypt')
+const session = require('express-session') // import session untuk menyimpan data ke dalam browser
+const flash = require('express-flash')
 
 const app = express()
 const port = 8000
@@ -7,16 +11,29 @@ const port = 8000
 app.set('view engine', 'hbs') // method untuk set view engine 
 app.use('/assets', express.static(__dirname + '/assets')) // path folder assets
 app.use(express.urlencoded({extended: false}))
+app.use(flash())
 
-const db = require('./connection/db')
+app.use(session({
+    secret: 'whatever',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 2 * 60 * 60 * 1000 // 2 hours
+    }
+}))
 
+
+app.get('/contact', function (request, response) {
+    response.render('contact')
+})
+    
+db.connect(function(err, client, done){
+        if (err) throw err // menampilkan error koneksi database
 
 
 app.get('/', function (request, response) {
 
-    
-    db.connect(function(err, client, done){
-        if (err) throw err // menampilkan error koneksi database
+    console.log(request.session);
         
         client.query('SELECT * FROM tb_projects ORDER BY id DESC', function (err, result) {
             if (err) throw err // menampilkan error dari queary
@@ -35,17 +52,20 @@ app.get('/', function (request, response) {
                 })
                 return {
                     ...item,
-                    duration: getDistanceTime(new Date(item.start_date), new Date(item.end_date))
+                    duration: getDistanceTime(new Date(item.start_date), new Date(item.end_date)),
+                    isLogin: request.session.isLogin
                 }
             })
-            response.render('index', {dataProject})
+            response.render('index', {dataProject, user: request.session.user, isLogin: request.session.isLogin})
          })
     })
 
-        
-})
 
 app.get('/add-project', function (request, response) {
+    if(!request.session.user){
+        request.flash('danger','You have to login first')
+    return response.redirect('login')
+    }
     response.render('addproject')
 })
 
@@ -57,9 +77,6 @@ app.post('/add-project', function (request, response) {
         inputProject: title,startDate: start,endDate: end,inputDescription: description,react,node,vuejs,js,inputImage: image
     } = request.body
     
-    db.connect(function(err, client, done){
-        if (err) throw err // menampilkan error pada query
-
        let query = `INSERT INTO tb_projects (project_name, start_date, end_date, description, technologies, image) VALUES 
         ('${title}','${start}','${end}','${description}', '{"${react}","${node}","${vuejs}","${js}"}', '${image}' )`
 
@@ -70,17 +87,12 @@ app.post('/add-project', function (request, response) {
         if (err) throw err // menampilkan error pada query
         response.redirect('/')
         })
-
-   })
 })
 
 app.get('/project-detail/:id', function (request, response) {
 
     let id = request.params.id
 
-    db.connect(function(err, client, done){
-        if (err) throw err // menampilkan error koneksi database
-        
         let query = `SELECT * FROM tb_projects WHERE id=${id}`
 
         client.query(query, function (err, result) {
@@ -107,14 +119,15 @@ app.get('/project-detail/:id', function (request, response) {
             })
             response.render('project-detail',{data: dataProject[0]} )
          })
-    })
 })
 
 app.get('/edit-project/:id', function (request, response) {
-    let id = request.params.id
+    if(!request.session.user){
+        request.flash('danger','You have to login first')
+    return response.redirect('login')
+    }
 
-    db.connect(function(err, client, done){
-        if (err) throw err // menampilkan error koneksi database
+    let id = request.params.id
         
         let query = `SELECT * FROM tb_projects WHERE id=${id}`
 
@@ -126,7 +139,6 @@ app.get('/edit-project/:id', function (request, response) {
 
             response.render('edit-project',{data: data[0]} )
          })
-    })
 }) 
 
 app.post('/edit-project/:id', function (request, response) {
@@ -136,9 +148,6 @@ app.post('/edit-project/:id', function (request, response) {
         inputProject: title,startDate: start,endDate: end,inputDescription: description,react,node,vuejs,js,inputImage: image
     } = request.body
     
-    db.connect(function(err, client, done){
-        if (err) throw err // menampilkan error pada query
-
        let query = `UPDATE tb_projects
 	SET project_name='${title}', start_date='${start}', end_date='${end}', description='${description}', technologies='{${react},${node},${vuejs},${js}}', image = '${image}' WHERE id=${id}`
 
@@ -160,17 +169,12 @@ app.post('/edit-project/:id', function (request, response) {
 
         response.redirect('/')
         })
-
-   })
     
 })
 
 app.get('/delete-project/:id', function (request, response) {
     let id = request.params.id
 
-    db.connect(function(err, client, done){
-        if (err) throw err // menampilkan error koneksi database
-        
         let query = `DELETE FROM tb_projects WHERE id=${id}`
 
         client.query(query, function (err, result) {
@@ -178,14 +182,85 @@ app.get('/delete-project/:id', function (request, response) {
             
             response.redirect('/')
          })
+})
+
+app.get('/register', function (request, response) {
+response.render('register')
+})
+
+app.post('/register', function(request,response){
+
+    let { username: name, password: pass , email} = request.body
+
+    const hashedPassword = bcrypt.hashSync(pass, 10)
+
+    db.connect(function(err, client, done){
+        if (err) throw err // menampilkan error koneksi database
+        
+        let query = `INSERT INTO public.tb_user(name, email, password)
+            VALUES ('${name}', '${email}', '${hashedPassword}');`
+
+        client.query(query, function (err, result) {
+            if (err) throw err // menampilkan error dari queary
+            
+            let data = result.rows
+            // console.log(data);
+
+            response.redirect('register')
+         })
     })
 })
 
-
-app.get('/contact', function (request, response) {
-response.render('contact')
+app.get('/login', function(request,response){
+    response.render('login')
 })
 
+app.post('/login', function(request,response){
+
+    let = {inputEmail: email, inputPassword: pass} = request.body
+
+    let query = `SELECT * FROM tb_user WHERE email='${email}'`
+
+    client.query(query, function(err,result){
+        if (err) throw err // menampilkan error query
+
+        // console.log(result.rows[0]);
+        if (result.rows.length == 0) {
+            console.log('email belum terdaftar')
+            request.flash('danger', 'Email is not register')
+
+        return response.redirect('login')
+        } 
+
+    const isMatch = bcrypt.compareSync(pass, result.rows[0].password)
+
+    console.log(isMatch)
+
+    if (isMatch) {
+        console.log('Login Success !');
+
+        request.session.isLogin =true
+        request.session.user = {
+            id: result.rows[0].id,
+            name: result.rows[0].name,
+            email: result.rows[0].email
+        }
+        request.flash('success','Login Succesful')
+        response.redirect('/')
+    } else {
+        console.log('Login Failed');
+        response.redirect('/login')
+    }
+    })
+})
+
+app.get('/logout', function(request,response){
+    request.session.destroy()
+
+    response.redirect('/login')
+})
+
+})
 
 
 function getFullTime(time) {
